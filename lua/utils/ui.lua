ebb3ac5b4f3ffbe237d30c594c1740fc7445cab3
Teepob Harutaipree <Teepob.Harutaipree@agoda.com>
2024-07-18 18:30:26 +0700

    telescope zoxide and lazygit_nvim open

--- lua/utils/ui.lua
+++ lua/utils/ui.lua
@@ -19,4 +19,33 @@ function M.foldexpr()
   return vim.b[buf].ts_folds and vim.treesitter.foldexpr() or "0"
 end
 
+----- MY ADDED ------------
+
+---@return {fg?:string}?
+function M.fg(name)
+  local color = M.color(name)
+  return color and { fg = color } or nil
+end
+
+---@param name string
+---@param bg? boolean
+---@return string?
+function M.color(name, bg)
+  ---@type {foreground?:number}?
+  ---@diagnostic disable-next-line: deprecated
+  local hl = vim.api.nvim_get_hl and vim.api.nvim_get_hl(0, { name = name, link = false })
+    or vim.api.nvim_get_hl_by_name(name, true)
+  ---@diagnostic disable-next-line: undefined-field
+  ---@type string?
+  local color = nil
+  if hl then
+    if bg then
+      color = hl.bg or hl.background
+    else
+      color = hl.fg or hl.foreground
+    end
+  end
+  return color and string.format("#%06x", color) or nil
+end
+
 return M

68f6c9963224841c523693e1157e9742301f3787
Teepob Harutaipree <Teepob.Harutaipree@agoda.com>
2024-08-20 20:36:19 +0700

    neotree: remove empty config (fix setup)

--- lua/plugins/extras/neotree.lua
+++ lua/plugins/extras/neotree.lua
@@ -278,29 +278,29 @@ return {
       )
       return opts
     end,
-    config = function(_, opts)
-      -- local function on_move(data)
-      --   LazyVim.lsp.on_rename(data.source, data.destination)
-      -- end
+    -- config = function(_, opts)
+    -- local function on_move(data)
+    --   LazyVim.lsp.on_rename(data.source, data.destination)
+    -- end
 
-      -- local events = require("neo-tree.events")
-      -- opts.event_handlers = opts.event_handlers or {}
-      -- vim.list_extend(opts.event_handlers, {
-      --   { event = events.FILE_MOVED, handler = on_move },
-      --   { event = events.FILE_RENAMED, handler = on_move },
-      -- })
+    -- local events = require("neo-tree.events")
+    -- opts.event_handlers = opts.event_handlers or {}
+    -- vim.list_extend(opts.event_handlers, {
+    --   { event = events.FILE_MOVED, handler = on_move },
+    --   { event = events.FILE_RENAMED, handler = on_move },
+    -- })
 
-      -- require("neo-tree").setup(opts)
-      --   reload neotree git status when lazygit closed
-      --   vim.api.nvim_create_autocmd("TermClose", {
-      --     pattern = "*lazygit",
-      --     callback = function()
-      --       if package.loaded["neo-tree.sources.git_status"] then
-      --         require("neo-tree.sources.git_status").refresh()
-      --       end
-      --     end,
-      --   })
-      -- end,
-    end,
+    -- require("neo-tree").setup(opts)
+    --   reload neotree git status when lazygit closed
+    --   vim.api.nvim_create_autocmd("TermClose", {
+    --     pattern = "*lazygit",
+    --     callback = function()
+    --       if package.loaded["neo-tree.sources.git_status"] then
+    --         require("neo-tree.sources.git_status").refresh()
+    --       end
+    --     end,
+    --   })
+    -- end,
+    -- end,
   },
 }

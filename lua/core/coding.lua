57299b3e83364b331987e267dbbf98134fecd911
Teepob Harutaipree <Teepob.Harutaipree@agoda.com>
2024-08-23 18:41:17 +0700

    disable tailwind

--- lua/core/coding.lua
+++ lua/core/coding.lua
@@ -139,7 +139,7 @@ return {
         -- markdown
         "markdownlint",
         -- rustywind for tailwindcss
-        "rustywind",
+        -- "rustywind",
         -- astro
         "astro-language-server",
         -- Solidity

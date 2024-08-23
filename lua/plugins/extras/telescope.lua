ca48d7e90604195c2aea174c079918b0ddc120a1
Teepob Harutaipree <Teepob.Harutaipree@agoda.com>
2024-08-23 13:52:55 +0700

    fix: telescope not loaded, keymap esc enhance

--- lua/plugins/extras/telescope.lua
+++ lua/plugins/extras/telescope.lua
@@ -269,39 +269,41 @@ return {
   { import = "plugins.extras.telescope-zoxide" },
   {
     "nvim-telescope/telescope.nvim",
-    opts = {
-      defaults = {
-        prompt_prefix = "   ",
-        initial_mode = "insert",
-        selection_strategy = "reset",
-        sorting_strategy = "ascending",
-        layout_strategy = "horizontal",
-        layout_config = {
-          horizontal = {
-            prompt_position = "top",
-            preview_width = 0.55,
-            results_width = 0.8,
+    opts = function()
+      return {
+        defaults = {
+          prompt_prefix = "   ",
+          initial_mode = "insert",
+          selection_strategy = "reset",
+          sorting_strategy = "ascending",
+          layout_strategy = "horizontal",
+          layout_config = {
+            horizontal = {
+              prompt_position = "top",
+              preview_width = 0.55,
+              results_width = 0.8,
+            },
+            vertical = {
+              mirror = false,
+            },
+            width = 0.87,
+            height = 0.80,
+            preview_cutoff = 120,
           },
-          vertical = {
-            mirror = false,
+          path_display = { "truncate" },
+          mappings = {
+            i = {
+              ["<C-p>"] = require("telescope.actions.layout").toggle_preview,
+              ["X"] = require("telescope.actions").delete_buffer,
+              ["<C-u>"] = false, -- disable scroll preview - to use clear prompt
+            },
+            n = {
+              ["<C-p>"] = require("telescope.actions.layout").toggle_preview,
+            },
           },
-          width = 0.87,
-          height = 0.80,
-          preview_cutoff = 120,
         },
-        path_display = { "truncate" },
-        mappings = {
-          i = {
-            -- ["C-v"] = open_selected_file_in_vertical,
-            ["<C-p>"] = require("telescope.actions.layout").toggle_preview,
-          },
-          n = {
-            ["X"] = require("telescope.actions").delete_buffer,
-            ["<C-p>"] = require("telescope.actions.layout").toggle_preview,
-          },
-        },
-      },
-    },
+      }
+    end,
     keys = {
       -- add <leader>fa to find all, including hidden files
       -- {

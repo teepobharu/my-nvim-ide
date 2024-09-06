74a138a3735eea67f652f1d0442d687308dc7b2e
Teepob Harutaipree <Teepob.Harutaipree@agoda.com>
2024-09-07 02:09:45 +0700

    feat: code-runner pipenv python support, copilot lama alternative

--- lua/langs/mypython.lua
+++ lua/langs/mypython.lua
@@ -1,3 +1,14 @@
 -- https://stackoverflow.com/a/75509259
 -- after doing this works in ide correctly
 -- echo '{ "venvPath": ".", "venv": ".venv" }' >> pyrightconfig.json
+--
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "python",
+      },
+    },
+  },
+}

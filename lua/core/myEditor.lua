4145fd9b944d82de9231c5a46a86e97f24fe509f
Teepob Harutaipree <Teepob.Harutaipree@agoda.com>
2024-11-26 02:16:03 +0700

    feat(fzf-lua): update git openn remote

--- lua/core/myEditor.lua
+++ lua/core/myEditor.lua
@@ -1,4 +1,5 @@
-local pathUtils = require("utils.path")
+local pathUtil = require("utils.path")
+local gitUtil = require("utils.git")
 
 local mapping_key_prefix = vim.g.ai_prefix_key or "<leader>A" -- orginal from codecompanion.lua
 function sentSelectedToTerminal()
@@ -52,6 +53,27 @@ local isToggleCurrentLazyTerm = function(name, termOpts)
     lazygitTerm.term:toggle()
   end
 end
+
+---@param ref : string
+---@param mode "file" | "commit"
+local open_remote = function(ref, mode)
+  local file_path = vim.fn.expand("%:p")
+  local commit_hash = ref:match("%w+")
+  local line_number = vim.fn.line(".")
+
+  local gitroot = pathUtil.get_git_root()
+  local remote_path = gitUtil.get_remote_path("origin")
+  local git_file_path = file_path:gsub(gitroot .. "/?", "")
+  local url_pattern = "https://%s/blob/%s/%s#L%d"
+  local url = ""
+  if mode == "file" then
+    url = string.format(url_pattern, remote_path, commit_hash, git_file_path, line_number)
+  else
+    url = string.format("https://%s/commit/%s", remote_path, commit_hash)
+  end
+  vim.fn.jobstart({ "open", url }, { detach = true })
+end
+
 return {
   -- Disabled list
   { "nvimdev/dashboard-nvim", lazy = true, enabled = false },
@@ -59,11 +81,66 @@ return {
   -- folke/edgy.nvim:  https://github.com/LazyVim/LazyVim/blob/1f8469a53c9c878d52932818533ce51c27ded5b6/lua/lazyvim/plugins/extras/ui/edgy.lua#L97
   {
     "ibhagwan/fzf-lua",
+    opts = {
+
+      git = {
+        bcommits = {
+          actions = {
+            ["ctrl-o"] = function(selected)
+              -- Custom action to open remote file
+              local commit_hash = selected[1]:match("%w+")
+              open_remote(commit_hash, "file")
+              open_remote(commit_hash, "commit")
+            end,
+          },
+        },
+        blame = {
+          actions = {
+            ["ctrl-o"] = function(selected)
+              -- Custom action to open remote file
+              local commit_hash = selected[1]:match("%w+")
+              open_remote(commit_hash, "file")
+              open_remote(commit_hash, "commit")
+            end,
+          },
+        },
+        commits = {
+          actions = {
+            -- ["default"] = function(selected)
+            --   -- Default action (e.g., open commit diff)
+            -- end,
+            ["ctrl-o"] = function(selected)
+              -- Custom action to open remote file
+              local commit_hash = selected[1]:match("%w+")
+              open_remote(commit_hash, "file")
+              open_remote(commit_hash, "commit")
+              -- local file_path = vim.fn.expand("%:p")
+              -- local line_number = vim.fn.line(".")
+              --
+              -- local gitroot = pathUtil.get_git_root()
+              -- local remote_path = gitUtil.get_remote_path("origin")
+              -- local git_file_path = file_path:gsub(gitroot .. "/?", "")
+              -- local url_pattern = "https://%s/blob/%s/%s#L%d"
+              -- local url = string.format(url_pattern, remote_path, commit_hash, git_file_path, line_number)
+              -- vim.fn.jobstart({ "open", url }, { detach = true })
+              --
+              -- vim.cmd("e " .. file_path)
+            end,
+          },
+        },
+      },
+    },
     keys = {
       -- opts.desc = "Git branch FZF"
       -- keymap("n", "<localleader>gO", function()
       --   require("config.telescope_pickers").fzf.pickers.open_git_pickers_telescope()
       -- end, opts)
+      {
+        "<leader>gS",
+        "<cmd> :FzfLua git_blame<CR>",
+        desc = "FZF Git Blame",
+        mode = "n",
+      },
       {
         "<leader>go",
         function()
@@ -391,7 +468,7 @@ return {
         -- cd ~/.cache && pipenv --python 3
         -- pipenv install pandas
         python = {
-          pathUtils.get_pythonpath(true) .. " -u",
+          pathUtil.get_pythonpath(true) .. " -u",
           -- first check if therre is virt env in the git rroot dir or .venv or not if not python3 -u else pipenv run python -u
           -- purre cli handle not work with handling https://github.com/jellydn/quick-code-runner.nvim/blob/main/lua/quick-code-runner/utils.lua#L248
           -- "[[ -d .venv ]] && echo 'pipenv run python -u' || echo 'python3 -u'", -- not work
@@ -419,13 +496,13 @@ return {
         },
       },
       global_files = {
-        javascript = pathUtils.get_global_file_by_type("js"),
-        typescript = pathUtils.get_global_file_by_type("ts"),
-        python = pathUtils.get_global_file_by_type("py"),
-        go = pathUtils.get_global_file_by_type("go"),
-        lua = pathUtils.get_global_file_by_type("lua"),
+        javascript = pathUtil.get_global_file_by_type("js"),
+        typescript = pathUtil.get_global_file_by_type("ts"),
+        python = pathUtil.get_global_file_by_type("py"),
+        go = pathUtil.get_global_file_by_type("go"),
+        lua = pathUtil.get_global_file_by_type("lua"),
         --  end common -------------------
-        sh = pathUtils.get_global_file_by_type("sh"),
+        sh = pathUtil.get_global_file_by_type("sh"),
       },
     },
   },
